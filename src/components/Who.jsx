8acf8e42642778800bf097d4aba747c1dac8d498
freetimezp <kraftukrnet@ukr.net>
2023-03-12 15:49:19 +0200

    10. js-portfolio-09-3D. 3D cube.

--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { OrbitControls } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import Cube from './Cube';
+
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
@@ -75,6 +79,13 @@ const Who = () => {
             <Container>
                 <Left>
                     {/* 3D model */}
+                    <Canvas camera={{fov: 25, position: [5, 5, 5]}}>
+                        <OrbitControls enableZoom={false} autoRotate />
+                        <ambientLight intensity={1} />
+                        <directionalLight position={[3, 2, 1]} />
+
+                        <Cube />
+                    </Canvas>
                 </Left>
                 <Right>
                     <Title>Think outside the square space.</Title>
